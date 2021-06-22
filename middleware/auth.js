8afa65e1cdf97615b7aef67fa04e5eb3dd721d7f
aliwafa16 <aliwafamuhamad16@gmail.com>
2021-06-22 21:52:59 +0700

    asas

--- middleware/auth.js
+++ middleware/auth.js
@@ -27,6 +27,65 @@ const Registrasi_Controller = {
             console.log(err);
             response.error(res, { error: err.message });
         }
+    },
+    signIn : async (req, res) => {
+        var post = {
+            email_user:req.body.email_user,
+            password_user:req.body.password_user
+        }
+
+        var query = "SELECT * FROM ?? WHERE ??=? AND ??==?";
+        var table = ["tbl_user", "email_user", md5[post.email_user], post.email_user];
+
+
+        query = mysql.format(query, table);
+        connection.query(query, function(error, rows){
+            if(error){
+                console.log(error);
+            }else {
+                if(rows.lenght==1){
+                    var token = jwt.sign({rows}, config.secret, {
+                        expiresIn:1444
+                    });
+                    id_user = rows[0].id_user;
+                    var data = {
+                        id_user : id_user,
+                        akses_token : token,
+                        ip_address : ip.address()
+                    }
+
+                    var query = "INSERT INTO ?? SET ?";
+                    var table = ['tbl_akses_token'];
+
+                    query = mysql.format(query,table);
+                    connection.query(query, data, function (error, rows) {
+                        if(error){
+                            console.log(error);
+                        }else {
+                            response.success(res, {
+                                message:'Token tergenerate !!!',
+                                token : token,
+                                currUser : data.id_user
+                                })
+                        }
+                    });
+                }else{
+                    response.success(res, {message: 'Email atau password Salah'})
+                }
+            }
+        })
+        // try {
+        //     const user = await UserModel.findOne({
+        //         where : {
+        //             [sequelize.Op.and]:[
+        //                 {email_user:req.body.email_user},
+        //                 {password_user:req.body.password_user}
+        //             ]
+        //         }
+        //     })
+        // }catch(err){
+
+        // }
     }
     
 }
