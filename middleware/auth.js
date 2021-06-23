0f2224c8399c57a9cc2d9915e05396805d1f1535
aliwafa16 <aliwafamuhamad16@gmail.com>
2021-06-23 09:11:53 +0700

    sss

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,6 @@ const jwt = require('jsonwebtoken');
 const key = ('../config/secret');
 const UserModel = require('../models/UserModel');
 const ip = require('ip');
-const bcrypt = require('bcrypt');
 
 const Registrasi_Controller = {
     signUp : async (req, res) => {
@@ -21,7 +20,7 @@ const Registrasi_Controller = {
                 await UserModel.create(req.body);
                 response.success(res, {message:'create data success !!'})
             }else{
-                response.success(res, {message: 'Email sudah digunakan'})
+                response.error(res, { error: 'Email Sudah digunakan !!' })
             }
         }catch(err){
             console.log(err);
@@ -29,65 +28,6 @@ const Registrasi_Controller = {
         }
     },
     signIn : async (req, res) => {
-<<<<<<< HEAD
-        var post = {
-            email_user:req.body.email_user,
-            password_user:req.body.password_user
-        }
-
-        var query = "SELECT * FROM ?? WHERE ??=? AND ??==?";
-        var table = ["tbl_user", "email_user", md5[post.email_user], post.email_user];
-
-
-        query = mysql.format(query, table);
-        connection.query(query, function(error, rows){
-            if(error){
-                console.log(error);
-            }else {
-                if(rows.lenght==1){
-                    var token = jwt.sign({rows}, config.secret, {
-                        expiresIn:1444
-                    });
-                    id_user = rows[0].id_user;
-                    var data = {
-                        id_user : id_user,
-                        akses_token : token,
-                        ip_address : ip.address()
-                    }
-
-                    var query = "INSERT INTO ?? SET ?";
-                    var table = ['tbl_akses_token'];
-
-                    query = mysql.format(query,table);
-                    connection.query(query, data, function (error, rows) {
-                        if(error){
-                            console.log(error);
-                        }else {
-                            response.success(res, {
-                                message:'Token tergenerate !!!',
-                                token : token,
-                                currUser : data.id_user
-                                })
-                        }
-                    });
-                }else{
-                    response.success(res, {message: 'Email atau password Salah'})
-                }
-            }
-        })
-        // try {
-        //     const user = await UserModel.findOne({
-        //         where : {
-        //             [sequelize.Op.and]:[
-        //                 {email_user:req.body.email_user},
-        //                 {password_user:req.body.password_user}
-        //             ]
-        //         }
-        //     })
-        // }catch(err){
-
-        // }
-=======
         const email_user = req.body.email_user;
         const password_user = req.body.password_user;
         const user = await UserModel.findOne({
@@ -109,7 +49,6 @@ const Registrasi_Controller = {
         }else{
             response.success(res, {message: 'Email Salah !!'})
         }
->>>>>>> origin/master
     }
 }
 
