5bb9e37dfca5fcd367df58758df34a65cd56befa
aliwafa16 <aliwafamuhamad16@gmail.com>
2021-06-25 20:31:00 +0700

    tambahan compare password

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,12 @@
 const database = require('../config/database');
 const sequelize = require('sequelize');
-const md5 = require('md5');
+const md5 = require('js-md5');
 const response = require('../helpers/respons-parser');
 const jwt = require('jsonwebtoken');
 const key = ('../config/secret');
 const UserModel = require('../models/UserModel');
 const ip = require('ip');
+const bcrypt = require('bcrypt');
 
 const Registrasi_Controller = {
     signUp : async (req, res) => {
@@ -28,27 +29,35 @@ const Registrasi_Controller = {
         }
     },
     signIn : async (req, res) => {
-        const email_user = req.body.email_user;
-        const password_user = req.body.password_user;
-        const user = await UserModel.findOne({
-            where : {
-                email_user : email_user
-            }
-        });
-        if(user){
-            const password = await md5(password_user, user.password_user);
-            if(password){
-                const data = {
-                    id : user.id_user
+
+        try{
+            const email_user = req.body.email_user;
+            const password_user = req.body.password_user;
+
+                console.log(email_user);
+                const user = await UserModel.findOne({
+                    where : {
+                        email_user : email_user
+                    }
+                });
+                if(user){
+                    if(md5(password_user)==user.password_user){
+                        const data = {
+                            id : user.id_user
+                        }
+                        const token = await jwt.sign({data}, key);
+                        response.success(res, {data:{token:token,id_user:user.id_user,role_id:user.role_id,username:user.username}})
+                    }else{
+                        response.error(res, {error: 'Password Salah !!'})
+                    }
+                }else{
+                    response.error(res, {error: 'Email user tidak ditemukan !!'})
                 }
-                const token = await jwt.sign({data}, key);
-                response.success(res, {data:token})
-            }else{
-                response.success(res, {message: 'Password Salah !!'})
-            }
-        }else{
-            response.success(res, {message: 'Email Salah !!'})
+        }catch(err){
+            console.log(err);
+            response.error(res, { error: err.message });
         }
+        
     }
 }
 
